2135c97b66df97f6639a6c6c8bb5b761fd739b41
MEHMET YİLDİZ <yildiz655@gmail.com>
2024-05-10 00:15:37 +0300

    Feature-[1.0.1.2] : release bat updated

--- release.bat
+++ release.bat
@@ -1,20 +1,29 @@
 @Echo Off
 CD /D "C:\Patientcare\Patientcare\Auth"
+Call npm install
 Call npm run build 
 CD /D "C:\Patientcare\Patientcare\Business"
+Call npm install
 Call npm run build
 CD /D "C:\Patientcare\Patientcare\File"
+Call npm install
 Call npm run build
 CD /D "C:\Patientcare\Patientcare\Log"
+Call npm install
 Call npm run build
 CD /D "C:\Patientcare\Patientcare\Setting"
+Call npm install
 Call npm run build
 CD /D "C:\Patientcare\Patientcare\System"
+Call npm install
 Call npm run build
 CD /D "C:\Patientcare\Patientcare\Userrole"
+Call npm install
 Call npm run build
 CD /D "C:\Patientcare\Patientcare\Warehouse"
+Call npm install
 Call npm run build
 CD /D "C:\Patientcare\Patientcare\Web"
+Call npm install
 Call npm run build
 CD /D "C:\Patientcare\Patientcare"
